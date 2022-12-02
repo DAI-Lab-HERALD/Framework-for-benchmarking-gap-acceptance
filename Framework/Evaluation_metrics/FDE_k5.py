65acb2dc7379c8cd5c83a8d159621f4271c17091
Julian Schumann <julian.f.schumann@web.de>
2022-12-02 16:16:12 +0000

    Remove error in FDE

--- Framework/Evaluation_metrics/FDE_k5.py
+++ Framework/Evaluation_metrics/FDE_k5.py
@@ -9,13 +9,12 @@ class FDE_k5(evaluation_template):
     def evaluate_prediction_method(self):
         Error = 0
         for i_sample in range(len(self.Output_path_pred)):
-            (num_poss, num_timesteps_out) = self.Output_path_pred.iloc[i_sample, 0].shape
-            diff = np.zeros((num_poss, num_timesteps_out))
+            (num_poss, _) = self.Output_path_pred.iloc[i_sample, 0].shape
+            diff = np.zeros((num_poss,))
             for j in range(len(self.Output_path.columns)):
                 diff += (self.Output_path_pred.iloc[i_sample, j][:,-1] -
                          self.Output_path.iloc[i_sample, j][np.newaxis,-1]) ** 2
             diff = np.sqrt(diff)
-            diff = np.mean(diff, -1)
             index = np.argsort(diff)[:int(0.05 * num_poss)]
             Error += np.mean(diff[index])
         return [Error / len(self.Output_path_pred)]
